14f5192a20efe4976271e6f2435657f8c6f1e639
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-05-29 23:00:17 +0800

    created the pseudocode for the program

---
+++ quiz_maker_inheritance.py
@@ -0,0 +1,12 @@
+# creating a class for making a question
+    # input the question
+    # input the choices
+    # input the correct answer
+    # save inputted data to a dictionary
+# creating a class for the running the quiz maker
+    # initialize the question data as dictionary
+    # add the question data to the dictionary
+    # ask if user wants to make more questions
+    # function to save the quiz data
+# running the program
+# saving the data to a text file
