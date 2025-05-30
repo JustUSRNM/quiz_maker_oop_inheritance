bafb4152e95c8c72f396e2cc9f65e1b9f82f36e9
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-05-30 22:23:02 +0800

    added the class for reading and loading the text file

--- quiz_taker_inheritance.py
+++ quiz_taker_inheritance.py
@@ -1,5 +1,16 @@
 # import the essential modules
+import json
+import random
+import os
 # create a class for loading the quiz.json file
+class quiz_reader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.quiz_data = self.load_quiz()
+
+    def load_quiz(self):
+        with open(self.filename, 'r') as file:
+            return json.load(file)
 # create the class for the flow of the program 
     # pick a random question
     # display the question and the choices
