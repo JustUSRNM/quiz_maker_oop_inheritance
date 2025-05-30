e917a7a71d8b93ca79d2b0b03b1fd84a7a2e7508
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-05-30 22:12:11 +0800

    created the pseudo code for the program

---
+++ quiz_taker_inheritance.py
@@ -0,0 +1,9 @@
+# import the essential modules
+# create a class for loading the quiz.json file
+# create the class for the flow of the program 
+    # pick a random question
+    # display the question and the choices
+    # let the taker answer the question
+    # check the answer if it is correct
+    # display if they passed or not
+# running the program
